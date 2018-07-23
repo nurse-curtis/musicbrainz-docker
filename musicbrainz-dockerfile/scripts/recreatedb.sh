c480a37a1a217a921dac0697846d336d82c0cf4e
nurse-curtis <curtis.heffernan@gmail.com>
2018-07-22 23:25:46 -0600

    Update recreatedb.sh
    Updated to use variables

--- musicbrainz-dockerfile/scripts/recreatedb.sh
+++ musicbrainz-dockerfile/scripts/recreatedb.sh
@@ -3,4 +3,4 @@ eval $( perl -Mlocal::lib )
 
 FETCH_DUMPS=$1
 
-psql postgres -U musicbrainz -h db -c "DROP DATABASE musicbrainz;"; /createdb.sh $FETCH_DUMPS
+psql postgres -U "$POSTGRES_USER" -h "$POSTGRES_HOST" -c "DROP DATABASE $POSTGRES_DB;"; /createdb.sh $FETCH_DUMPS
